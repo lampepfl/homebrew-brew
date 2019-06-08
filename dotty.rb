d826316bb50281f867b949b81ce65f7bb48ad169
Anatolii <anatoliykmetyuk@gmail.com>
2019-06-08 21:59:21 +0200

    Update Dotty to 0.16.0-RC2

--- dotty.rb
+++ dotty.rb
@@ -1,8 +1,8 @@
 class Dotty < Formula
   desc "Experimental Scala Compiler"
   homepage "http://dotty.epfl.ch/"
-  url "https://github.com/lampepfl/dotty/releases/download/0.15.0-RC1/dotty-0.15.0-RC1.tar.gz"
-  sha256 "73d8aa11ef727a87c4359b4b2957510dc4cb958b366a6adee9095272c3334ca0"
+  url "https://github.com/lampepfl/dotty/releases/download/0.16.0-RC2/dotty-0.16.0-RC2.tar.gz"
+  sha256 "8ab7215bd015f363854dbc3f5635d275171cf569bdc992b0f8d1ff9feab2d1ad"
   # mirror "https://www.scala-lang.org/files/archive/scala-2.12.2.tgz"
 
   bottle :unneeded
