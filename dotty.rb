57958a6bb02faaf6e50961020b9336d0f014d852
Liu Fengyun <liu@fengy.me>
2020-11-30 18:12:35 +0100

    Fix name change and update to M2

--- dotty.rb
+++ dotty.rb
@@ -1,13 +1,13 @@
 class Dotty < Formula
   desc "Experimental Scala Compiler"
   homepage "http://dotty.epfl.ch/"
-  url "https://github.com/lampepfl/dotty/releases/download/3.0.0-M1/scala3-3.0.0-M1.tar.gz"
-  sha256 "3e92c7f72779a645af61fdd63da3f4073518ed3471e26aec553a6d858c1387d1"
+  url "https://github.com/lampepfl/dotty/releases/download/3.0.0-M2/scala3-3.0.0-M2.tar.gz"
+  sha256 "ec71104112749d0efdf1127f9c2ce9722f732181518be7e983a9f754a698c281"
   # mirror "https://www.scala-lang.org/files/archive/scala-2.12.2.tgz"
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk@8"
 
   def install
     rm_f Dir["bin/*.bat"]
@@ -15,7 +15,7 @@ class Dotty < Formula
   end
 
   test do
-    # test dotc and dotr:
+    # test scalac and scala:
     file = testpath/"Test.scala"
     file.write <<~EOS
       object Test {
@@ -25,20 +25,20 @@ class Dotty < Formula
       }
     EOS
 
-    shell_output("#{bin}/dotc #{file}")
-    out = shell_output("#{bin}/dotr Test").strip
+    shell_output("#{bin}/scalac #{file}")
+    out = shell_output("#{bin}/scala Test").strip
 
     assert_equal "4", out
 
-    # test dotd:
+    # test scalad:
     Dir.mkdir "#{testpath}/site"
     index_out = testpath/"site"/"index.md"
     index_out.write <<~EOS
     Hello, world!
     =============
     EOS
-    shell_output("#{bin}/dotd -siteroot #{testpath}/site -project Hello #{file}")
+    shell_output("#{bin}/scalad -siteroot #{testpath}/site -project Hello #{file}")
     index_file = File.open("#{testpath}/site/_site/index.html", "rb").read
-    assert index_file.include? '<h1><a href="#hello-world" id="hello-world">Hello, world!</a></h1>'
+    assert index_file.include? 'Hello, world!'
   end
 end
